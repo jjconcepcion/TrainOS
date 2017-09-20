f74f489f66bcf85b90ec2a3b1ef81abedc13e37c
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-09-20 16:26:06 -0700

    Implement add_ready_queue()
    PROCESS proc is inserted into the appropriate priority queue using
    helper method insert_ready_queue() which maintains proper double links.

--- kernel/dispatch.c
+++ kernel/dispatch.c
@@ -14,16 +14,44 @@ PCB *ready_queue [MAX_READY_QUEUES];
 
 
 
+/*
+ * insert_ready_queue
+ *----------------------------------------------------------------------------
+ * Insertion of proc into ready_queue maintains the doubly-linked list
+ */
+
+void insert_ready_queue(PROCESS proc) {
+    assert(proc->priority >= 0
+        && proc->priority < MAX_READY_QUEUES);
+    PROCESS queue_head, queue_tail;
+
+    queue_head = ready_queue[proc->priority];
+    if (queue_head == NULL) {
+    /* insert into empty queue */
+        ready_queue[proc->priority] = proc;
+        proc->prev = proc->next = proc;
+    } else {
+    /* insert into tail of queue */
+        queue_tail = queue_head->prev;
+        queue_tail->next = proc;
+        proc->prev = queue_tail;
+        proc->next = queue_head;
+        queue_head->prev = proc;
+    }
+}
 
 /*
  * add_ready_queue
  *----------------------------------------------------------------------------
- * The process pointed to by p is put the ready queue.
- * The appropiate ready queue is determined by p->priority.
+ * The process pointed to by proc is put the ready queue.
+ * The appropiate ready queue is determined by proc->priority.
  */
 
 void add_ready_queue (PROCESS proc)
 {
+    assert(proc != NULL);
+    proc->state = STATE_READY;
+    insert_ready_queue(proc);
 }
 
 
