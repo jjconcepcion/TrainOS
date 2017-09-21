ff2c4a20150a855648f2123ed26e8ea9ca9c87f0
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-09-20 21:44:19 -0700

    Implement create_process()
    Initializes PCB entry and process' 16KB stack frames which are allocated
    linearly from systems stack which starts 640KB memory address.

--- kernel/process.c
+++ kernel/process.c
@@ -10,6 +10,55 @@ PORT create_process (void (*ptr_to_new_proc) (PROCESS, PARAM),
 		     PARAM param,
 		     char *name)
 {
+	assert(next_free_pcb != NULL);
+	assert(prio >= 0 && prio <= MAX_READY_QUEUES);
+	PROCESS new_proc;
+	MEM_ADDR esp;
+
+	new_proc = next_free_pcb;
+	next_free_pcb = next_free_pcb->next;
+
+	/* Initiliaze PCB entry */
+	new_proc->magic 		= MAGIC_PCB;
+	new_proc->used 			= TRUE;
+	new_proc->state 		= STATE_READY;
+	new_proc->priority		= prio;
+	new_proc->first_port	= NULL;
+	new_proc->name			= name;
+
+	/* Computes starting address of process' stack frame */
+	esp = 640 * 1024 - (new_proc - pcb) * 16 * 1024;
+
+	/* Initialize stack frame */
+	esp -= 4;
+	poke_l(esp, param);				/* PARAM */
+	esp -= 4;
+	poke_l(esp, new_proc);			/* self */
+	esp -= 4;
+	poke_l(esp, 0);					/* dummy return address*/
+	esp -= 4;
+	poke_l(esp, ptr_to_new_proc);	/* EIP */
+	esp -= 4;
+	poke_l(esp, 0);					/* EAX */
+	esp -= 4;
+	poke_l(esp, 0);					/* ECX */
+	esp -= 4;
+	poke_l(esp, 0);					/* EDX */
+	esp -= 4;
+	poke_l(esp, 0);					/* EBX */
+	esp -= 4;
+	poke_l(esp, 0);					/* EBP */
+	esp -= 4;
+	poke_l(esp, 0);					/* ESI */
+	esp -= 4;
+	poke_l(esp, 0);					/* EDI */
+
+	/* Save process stack pointer */
+	new_proc->esp = esp;
+
+	add_ready_queue(new_proc);
+
+	return NULL;
 }
 
 
