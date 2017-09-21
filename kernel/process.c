d3e125def605fb9350438f45d0fa839188eea2fd
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-09-20 20:21:49 -0700

    Implement init_process()
    Initialize PCBs and use pcb[0] for boot process

--- kernel/process.c
+++ kernel/process.c
@@ -34,4 +34,18 @@ void print_all_processes(WINDOW* wnd)
 
 void init_process()
 {
+	/* Initialize boot process */
+	pcb[0].magic = MAGIC_PCB;
+	pcb[0].used = TRUE;
+	pcb[0].state = STATE_READY;
+	pcb[0].priority = 1;
+	pcb[0].name = "Boot process";
+	active_proc = pcb;
+
+	int i;
+	/* Initialize remaining PCBs */
+	for (i = 1; i < MAX_PROCS; i++) {
+		pcb[i].magic = 0;
+		pcb[i].used = FALSE;
+	}
 }
