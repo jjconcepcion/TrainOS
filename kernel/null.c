e0ff7600376273b930eab8deecdc5a3fdff5ea0a
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-11-24 23:32:17 -0800

    Implement Null process
    dispatch() assumes that there is at least one process on the ready queue.

--- kernel/null.c
+++ kernel/null.c
@@ -2,8 +2,13 @@
 #include <kernel.h>
 
 
+void null_process(PROCESS self, PARAM param)
+{
+    while(1);
+}
 
 
 void init_null_process()
 {
+    create_process (null_process, 0, 0, "Null process");
 }
