15e1c0050dcc1459eecf9235c84e8e1ffe0f4767
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-11-26 02:40:05 -0800

    Update shell implementation
    Properly handle leading backspace keyboard entries, and empty commands.

--- kernel/shell.c
+++ kernel/shell.c
@@ -3,6 +3,7 @@
 #define CMD_BUF_SIZE 128
 #define SHELL_CURSOR '$'
 #define ASCII_CR 13
+#define ASCII_BS 8
 
 void shell_process(PROCESS self, PARAM param);
 
@@ -29,7 +30,7 @@ void print_help(int window_id)
         "history   --print command history\n";
     static const char *help3 =
         "!<num>   --repeat command from history with given num\n"
-        "about    --print about message\n";
+        "about    --print about message";
 
     wm_print(window_id, "%s", help1);
     wm_print(window_id, "%s", help2);
@@ -39,7 +40,7 @@ void print_help(int window_id)
 
 void print_about(int window_id)
 {
-    static const *about = "\nTOS Shell \n\n(c) 2017, Jan Joseph Concepcion\n";
+    static const *about = "\nTOS Shell \n\n(c) 2017, Jan Joseph Concepcion";
     wm_print(window_id, "%s", about);
 }
 
@@ -101,21 +102,24 @@ void execute_cmd(char *cmd_buffer, int window_id)
     } else if (str_match(cmd, "cls")) {
         clear_shell(window_id);
     } else if (str_match(cmd, "shell")) {
+        start_shell();
     } else if (str_match(cmd, "pong")) {
+        start_pong();
     } else if (str_match(cmd, "ps")) {
     } else if (str_match(cmd, "history")) {
     } else if (*cmd == '!') {
     } else if (str_match(cmd, "about")) {
         print_about(window_id);
     } else {
-        wm_print(window_id, "\n%s: command not found\n", cmd);
+        if (*cmd != '\0')
+            wm_print(window_id, "\n%s: command not found", cmd);
     }
 }
 
 
 void shell_process(PROCESS self, PARAM param)
 {
-    int window_id;
+    int window_id, bytes_read;
     char *cmd_buffer, *ch, *cmd;
 
     window_id = wm_create(10, 3, 50, 17);
@@ -125,19 +129,38 @@ void shell_process(PROCESS self, PARAM param)
     /* allocate input buffer*/
     cmd_buffer = malloc(CMD_BUF_SIZE);
     ch = cmd_buffer;
+    bytes_read = 0;
     /* Parse each character typed */
     while (1) {
-        *ch = keyb_get_keystroke(window_id, TRUE);
-        /* Parsed complete line */
-        if (*ch == ASCII_CR) {
-            *(ch) = '\0'; /* discard carriage return */
-            execute_cmd(cmd_buffer, window_id);
-            print_shell_prompt(window_id);
-            ch = cmd_buffer;
-            continue;
+        if (bytes_read < CMD_BUF_SIZE - 1) {
+            *ch = keyb_get_keystroke(window_id, TRUE);
+            switch (*ch) {
+            case ASCII_CR:
+            /* Parsed complete line */
+                *(ch) = '\0'; /* discard carriage return */
+                /* Skip emty lines */
+                if (bytes_read > 0) {
+                    execute_cmd(cmd_buffer, window_id);
+                    ch = cmd_buffer;
+                    bytes_read = 0;
+                }
+                wm_print(window_id, "\n");
+                print_shell_prompt(window_id);
+                continue;
+            case ASCII_BS:
+            /* discard leading backspace */
+                if (bytes_read > 0) {
+                    wm_print(window_id, "%c", *ch);
+                    bytes_read--;
+                    ch--;
+                }
+                continue;
+            }
+            /* output character read */
+            wm_print(window_id, "%c", *ch);
+            ch++;
+            bytes_read++;
         }
-        wm_print(window_id, "%c", *ch);
-        ch++;
     }
     free(cmd_buffer);
 }
