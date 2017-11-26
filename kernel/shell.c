10affe8fda95f65a6b81fdc5cdbe651f0e772634
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-11-26 12:07:19 -0800

    Start implementation of command history
    Store command history in array of CMD_HIST_ENTRY which contains command
    string and command no. Treat array as circular which wraps around when
    command count exceeds array size.

--- kernel/shell.c
+++ kernel/shell.c
@@ -1,11 +1,21 @@
 #include <kernel.h>
 
 #define CMD_BUF_SIZE 128
+#define CMD_HIST_SIZE 32
 #define SHELL_CURSOR '$'
 #define ASCII_CR 13
 #define ASCII_BS 8
+#define CMD_HIST_INDEX(cmd_no) cmd_no % CMD_HIST_SIZE - 1
+
+
+typedef struct _CMD_HIST_ENTRY {
+    char *cmd;
+    int no;
+} CMD_HIST_ENTRY;
+
 
 void shell_process(PROCESS self, PARAM param);
+
 
 void print_shell_heading(int window_id)
 {
@@ -17,6 +27,7 @@ void print_shell_prompt(int window_id)
 {
     wm_print(window_id, "%c ", SHELL_CURSOR);
 }
+
 
 void print_help(int window_id)
 {
@@ -43,6 +54,7 @@ void print_about(int window_id)
     static const *about = "\nTOS Shell \n\n(c) 2017, Jan Joseph Concepcion";
     wm_print(window_id, "%s", about);
 }
+
 
 void clear_shell(int window_id)
 {
@@ -72,6 +84,22 @@ int str_match(const char *str1, const char *str2)
 
 
 /*
+ * Returns the decimal value ascii encoded unsiged number
+ */
+int atoi(char *str) {
+        int val;
+
+        val = 0;
+        while (*str != '\0') {
+                val = val * 10 + (int) (*str - '0');
+                str++;
+        }
+
+        return val;
+}
+
+
+/*
  * Inserts null terminator into buffer to delimit the command name from command
  * arguments. Returns pointer to string containing the command name.
  */
@@ -91,7 +119,7 @@ char* parse_cmd(char *buf)
     return cmd;
 }
 
-void execute_cmd(char *cmd_buffer, int window_id)
+char* execute_cmd(char *cmd_buffer, int window_id, CMD_HIST_ENTRY *cmd_history)
 {
     char *cmd;
 
@@ -107,6 +135,12 @@ void execute_cmd(char *cmd_buffer, int window_id)
         start_pong();
     } else if (str_match(cmd, "ps")) {
     } else if (str_match(cmd, "history")) {
+        int i = 0;
+        while (cmd_history[i].cmd != NULL && i < CMD_HIST_SIZE){
+            wm_print(window_id, "\n   %d  %s", cmd_history[i].no
+                                            , cmd_history[i].cmd);
+            i++;
+        }
     } else if (*cmd == '!') {
     } else if (str_match(cmd, "about")) {
         print_about(window_id);
@@ -114,21 +148,29 @@ void execute_cmd(char *cmd_buffer, int window_id)
         if (*cmd != '\0')
             wm_print(window_id, "\n%s: command not found", cmd);
     }
+
+    return cmd;
 }
 
 
 void shell_process(PROCESS self, PARAM param)
 {
-    int window_id, bytes_read;
+    CMD_HIST_ENTRY cmd_history[CMD_HIST_SIZE];
+    int window_id, bytes_read, i, cmd_no;
     char *cmd_buffer, *ch, *cmd;
 
     window_id = wm_create(10, 3, 50, 17);
     print_shell_heading(window_id);
     print_shell_prompt(window_id);
 
+    /* Initialize command history */
+    for (i = 0; i < CMD_HIST_SIZE; i++)
+        cmd_history[i].cmd = NULL;
+
     /* allocate input buffer*/
     cmd_buffer = malloc(CMD_BUF_SIZE);
     ch = cmd_buffer;
+    cmd_no = 1;
     bytes_read = 0;
     /* Parse each character typed */
     while (1) {
@@ -140,7 +182,11 @@ void shell_process(PROCESS self, PARAM param)
                 *(ch) = '\0'; /* discard carriage return */
                 /* Skip emty lines */
                 if (bytes_read > 0) {
-                    execute_cmd(cmd_buffer, window_id);
+                    cmd = execute_cmd(cmd_buffer, window_id, cmd_history);
+                    cmd_history[CMD_HIST_INDEX(cmd_no)].cmd = cmd;
+                    cmd_history[CMD_HIST_INDEX(cmd_no)].no = cmd_no;
+                    cmd_no++;
+                    cmd_buffer = malloc(CMD_BUF_SIZE);
                     ch = cmd_buffer;
                     bytes_read = 0;
                 }
@@ -163,6 +209,10 @@ void shell_process(PROCESS self, PARAM param)
         }
     }
     free(cmd_buffer);
+    /* deallocate command history */
+    for (i = 0; i < CMD_HIST_SIZE; i++)
+        if (cmd_history[i].cmd)
+            free(cmd_history[i].cmd);
 }
 
 
