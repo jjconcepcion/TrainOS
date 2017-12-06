9ac4a3f4889ecfca0a9a0038148d0fbc90f203f2
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-12-06 14:56:46 -0800

    Add function description comments
    Detail function results and assumed command formatting
    
    Change change_direction() prototype since our scenario allows control of
    a single train car.

--- kernel/train.c
+++ kernel/train.c
@@ -25,6 +25,9 @@ void strcat(char *dest, char *src)
 }
 
 
+/*
+ * Handles procedure for sending commands to model train via serial port
+ */
 void send_train_command(char *cmd, char *response, int response_len)
 {
     COM_Message msg;
@@ -37,7 +40,11 @@ void send_train_command(char *cmd, char *response, int response_len)
     send(com_port, &msg);
 }
 
-
+/*
+ * Change setting of model train track switch.
+ * command format: "M{id}{setting}\015"
+ *      where {id} is the switch id and {setting} is either R or G
+ */
 void set_switch(char *id, char *setting)
 {
     char cmd[BUFFER_SIZE];
@@ -52,7 +59,11 @@ void set_switch(char *id, char *setting)
     send_train_command(cmd, 0, 0);
 }
 
-
+/*
+ * Change the speed of the model train which we control
+ * command format: "L{TRAIN_ID}S#\015"
+ *      where {TRAIN_ID} is 20 and # is a number 0-5
+ */
 void set_train_speed(char *speed)
 {
     char cmd[BUFFER_SIZE];
@@ -68,8 +79,12 @@ void set_train_speed(char *speed)
     send_train_command(cmd, 0, 0);
 }
 
-
-void change_direction(char *train_id)
+/*
+ * Change the direction of travel of the model train which we control
+ * command format: "L{TRAIN_ID}D\015"
+ *      where {TRAIN_ID} is 20
+ */
+void change_direction()
 {
     char cmd[BUFFER_SIZE];
 
@@ -84,6 +99,10 @@ void change_direction(char *train_id)
 }
 
 
+/*
+ * Clears the memory buffer of the s88 which controls the model train contacts
+ * command format: "R\015"
+ */
 void clear_s88_buffer()
 {
     char cmd[BUFFER_SIZE];
@@ -95,6 +114,14 @@ void clear_s88_buffer()
     send_train_command(cmd, 0, 0);
 }
 
+
+/*
+ * Probes a track segment sensor indicating the presence of some vehicle
+ * command format: "C{id}\015"
+ *      where {id} is the contact id
+ * response format: "*X\015"
+ *      where X is 1 if a vehicle is present, otherwise X is 0
+ */
 int status_of_contact(char *id)
 {
     static const int len = 3;
@@ -118,6 +145,10 @@ int status_of_contact(char *id)
 }
 
 
+/*
+ * Configures track switches to bypass dead-end track segment to create a route
+ * loop
+ */
 void set_outer_loop_switches()
 {
     set_switch("5\0", "G\0");
@@ -135,7 +166,7 @@ void train_process(PROCESS self, PARAM param)
     window_id = wm_create(5, 5, 40, 15);
     wm_print(window_id, "TOS Train Application\n");
 
-    /* Ensure rogue train doesn't run of track */
+    /* Ensure rogue train if present doesn't run of track */
     set_outer_loop_switches();
 
     while(1) {
